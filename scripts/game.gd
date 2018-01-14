943f0decfa4b3ee7c4e23b7956eb4780e8ccc66a
Anderson Madeira <andersonmadeiracs@gmail.com>
2018-01-14 17:25:26 -0300

    Saving best score

--- scripts/game.gd
+++ scripts/game.gd
@@ -17,6 +17,8 @@ signal score_best_changed
 
 func _ready():
 	stage_manager.connect("stage_changed", self, "on_stage_changed")
+	connect("score_best_changed", self, "on_score_best_changed")
+	self.load_game()
 	pass
 	
 func on_stage_changed():
@@ -32,4 +34,37 @@ func _set_score_best(new_value):
 	if new_value > score_best:
 		score_best = new_value
 		emit_signal("score_best_changed")
+	pass
+	
+func on_score_best_changed():
+	save_game()
+	pass
+	
+func get_save_data():
+	var save_dict = {
+		score_best=score_best
+	}
+	return save_dict
+	
+func save_game():
+	var savegame = File.new()
+	savegame.open("user://game.save", File.WRITE)
+	savegame.store_line(self.get_save_data().to_json())
+	savegame.close()
+	pass
+	
+func load_game():
+	var savegame = File.new()
+	
+	if !savegame.file_exists("user://game.save"):
+		return
+		
+	var currentline = {}
+	savegame.open("user://game.save", File.READ)
+	currentline.parse_json(savegame.get_line())
+	
+	if currentline.has("score_best"):
+		score_best = currentline["score_best"]
+	
+	savegame.close()
 	pass
